587359700af0e6c4a1ea4dfe902863e280ef9baf
FarzadVav <vav.progg.ir@gmail.com>
2023-11-02 00:12:05 -0700

    add new header

--- javascript/index.js
+++ javascript/index.js
@@ -18,28 +18,38 @@ const changeThemeHandler = () => {
 const changeThemeToggleIcon = () => {
   if (theme === LIGHT) {
     toggle.innerHTML = `
-      <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
-        <path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z" />
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+        <path d="M14.828 14.828a4 4 0 1 0 -5.656 -5.656a4 4 0 0 0 5.656 5.656z" />
+        <path d="M6.343 17.657l-1.414 1.414" />
+        <path d="M6.343 6.343l-1.414 -1.414" />
+        <path d="M17.657 6.343l1.414 -1.414" />
+        <path d="M17.657 17.657l1.414 1.414" />
+        <path d="M4 12h-2" />
+        <path d="M12 4v-2" />
+        <path d="M20 12h2" />
+        <path d="M12 20v2" />
       </svg>
     `
   } else {
     toggle.innerHTML = `
-    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
-    <path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z" />
-    </svg>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+        <path d="M12 1.992a10 10 0 1 0 9.236 13.838c.341 -.82 -.476 -1.644 -1.298 -1.31a6.5 6.5 0 0 1 -6.864 -10.787l.077 -.08c.551 -.63 .113 -1.653 -.758 -1.653h-.266l-.068 -.006l-.06 -.002z" stroke-width="0" fill="currentColor" />
+      </svg>
     `
   }
 }
 
 // --- EVENTS
-// toggle.addEventListener('click', () => {
-//   if (theme === LIGHT) theme = DARK
-//   else theme = LIGHT
+toggle.addEventListener('click', () => {
+  if (theme === LIGHT) theme = DARK
+  else theme = LIGHT
 
-//   changeThemeHandler()
-// })
+  changeThemeHandler()
+})
 
-// window.addEventListener('load', () => {
-//   theme = localStorage.getItem('theme')
-//   changeThemeHandler()
-// })
+window.addEventListener('load', () => {
+  theme = localStorage.getItem('theme')
+  changeThemeHandler()
+})
