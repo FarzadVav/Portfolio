3dfaaee4a746720100473af78c2988b6d810ec57
FarzadVav <vav.progg.ir@gmail.com>
2023-12-19 05:20:19 -0800

    change dark mode stracture

--- javascript/theme.js
+++ javascript/theme.js
@@ -17,39 +17,21 @@ const changeThemeHandler = () => {
 
 const changeThemeToggleIcon = () => {
   if (theme === LIGHT) {
-    toggle.innerHTML = `
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-        <path d="M14.828 14.828a4 4 0 1 0 -5.656 -5.656a4 4 0 0 0 5.656 5.656z" />
-        <path d="M6.343 17.657l-1.414 1.414" />
-        <path d="M6.343 6.343l-1.414 -1.414" />
-        <path d="M17.657 6.343l1.414 -1.414" />
-        <path d="M17.657 17.657l1.414 1.414" />
-        <path d="M4 12h-2" />
-        <path d="M12 4v-2" />
-        <path d="M20 12h2" />
-        <path d="M12 20v2" />
-      </svg>
-    `
+    toggle.classList.remove('active')
   } else {
-    toggle.innerHTML = `
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="1.5" fill="none" stroke-linecap="round" stroke-linejoin="round">
-        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-        <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z" />
-      </svg>
-    `
+    toggle.classList.add('active')
   }
 }
 
 // --- Events
+window.addEventListener('load', () => {
+  theme = localStorage.getItem('theme')
+  changeThemeHandler()
+})
+
 toggle.addEventListener('click', () => {
   if (theme === LIGHT) theme = DARK
   else theme = LIGHT
 
-  changeThemeHandler()
-})
-
-window.addEventListener('load', () => {
-  theme = localStorage.getItem('theme')
   changeThemeHandler()
 })
